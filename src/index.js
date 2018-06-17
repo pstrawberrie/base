bcf042e34e72166703cb6e9563c46e828658b129
pstrawberrie <github_main@pstrawberrie.live>
2018-06-17 12:01:04 -0600

    tested on windows, working. minor adjustments

--- src/index.js
+++ src/index.js
@@ -6,9 +6,5 @@ import './public/scss/main.less';
 import { wussup } from './util/helpers';
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.warn('wtf');
   wussup();
-  var ele = document.createElement('div');
-  ele.className = 'testing';
-  document.appendChild(ele);
 });
