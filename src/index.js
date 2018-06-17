fe98c5e8500d4d4d78f0e761e1e1c397d5febf0b
pstrawberrie <github_main@pstrawberrie.live>
2018-06-17 09:16:31 -0600

    everything broke but...close to working

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@
  * App Entry
  */
 
-import './public/scss/main.scss';
+import './public/scss/main.less';
 import { wussup } from './util/helpers';
 
 document.addEventListener('DOMContentLoaded', function() {
+  console.warn('wtf');
   wussup();
+  var ele = document.createElement('div');
+  ele.className = 'testing';
+  document.appendChild(ele);
 });
---
+++ webpack.common.js
@@ -0,0 +1,86 @@
+/**
+ * Production Webpack Config
+ */
+
+const isProd = process.env.NODE_ENV === 'production';
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+
+  entry: './src/index.js',
+
+  output: {
+    filename: 'app-[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      // Scripts
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      },
+      // Styles
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { minimize: true || { discardComments: { removeAll: true } }}
+          },
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      // Images
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'fonts/[name].[ext]'
+            }
+          }
+        ]
+      },
+      // Fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app-[hash].css',
+      chunkFilename: '[id].css'
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+      minify: {removeComments: true, collapseWhitespace: true, conservativeCollapse: true}
+    })
+  ]
+
+};
